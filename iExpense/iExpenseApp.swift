e5c2595e4da4584a975316cf82ad8e26d08be4ec
adbak <adbak.dev@gmail.com>
2025-05-31 16:57:43 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Adnan Bakaev  on 31/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
